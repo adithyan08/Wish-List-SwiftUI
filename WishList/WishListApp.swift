3032edeedb09a69c1936710d3685e8b71803ee0d
Adithyan N A <itIos04@manappuram.com>
2025-06-08 22:18:34 +0530

    Initial Commit

---
+++ WishList/WishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishListApp.swift
+//  WishList
+//
+//  Created by adithyan na on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct WishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WishListTests/WishListTests.swift
@@ -0,0 +1,17 @@
+//
+//  WishListTests.swift
+//  WishListTests
+//
+//  Created by adithyan na on 8/6/25.
+//
+
+import Testing
+@testable import WishList
+
+struct WishListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
